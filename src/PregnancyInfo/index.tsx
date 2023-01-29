21e9cbb7701b8684f2537c306a9d36f98df7ff73
Panos Anastasiadis <anastapd@gmail.com>
2023-01-29 17:21:47 +0000

    implemented pregnancy info

--- src/PregnancyInfo/index.tsx
+++ src/PregnancyInfo/index.tsx
@@ -1,15 +1,22 @@
-import { PregnancyDate } from "../PregnancyInfoForm/PregnancyDate"
+import { PregnancyDate } from "../PregnancyInfoForm/PregnancyDate";
 
 interface PregnancyInfoProps {
-    info: PregnancyDate
+    info: PregnancyDate;
 }
 
 function PregnancyInfo(props: PregnancyInfoProps): JSX.Element {
+    const progress = props.info.progress();
     return (
-        <div>
-            {JSON.stringify(props.info.progress())}
+        <div className="card" style={{ width: "50em" }}>
+            <img className="card-img-top" src="./fetus.jpg" alt="Card image cap" />
+            <div className="card-body">
+                <h5 className="card-title">Congratulations on your pregnancy!!</h5>
+                <p className="card-text">
+                    Your pregnancy progress: <b>{progress.week} weeks</b> and <b>{progress.day} days</b>
+                </p>
+            </div>
         </div>
-    ) 
+    );
 }
 
-export default PregnancyInfo
+export default PregnancyInfo;
